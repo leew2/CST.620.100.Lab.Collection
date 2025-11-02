74fea4aa8001224e21ce4cfe03368c72f9ec2cd3
Wang Lee <leew2@csp.edu>
2025-11-02 15:09:09 -0600

    display adjustment

--- W1.L1.image_processing.py
+++ W1.L1.image_processing.py
@@ -5,13 +5,12 @@ import matplotlib.pyplot as plt
 def main():
     img_compare = []
     image = load_image("shareImage/cat.jpg", title="Cat")
-    img_compare.append(("Original Image", image))
-    #display_image(image, "Cat")
-
-    for kernel_size in [(3, 3), (5, 5), (7, 7)]:
-        guas = display_gaussian_blur(image, "Cat", kernel_size=kernel_size, display=False)
-        img_compare.append((f"Gaussian Blur {kernel_size}", guas))
-
+    img_compare.append(image[0])
+    display_image(image=image[0], title=image[1].__str__())
+    for kernel_size in [(5, 5),(11, 11),(15, 15)]:
+        guas = display_gaussian_blur(image=image[0], title='Gaussian Blur', kernel_size=kernel_size, display=False)
+        img_compare.append(guas[0])
+    display_image(img_compare, title="Comparison")
     
 
 def load_image(file_path, title="Image"):
@@ -23,18 +22,31 @@ def load_image(file_path, title="Image"):
 
 def display_image(image, title="Image"):
     if image is not None:
-        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-        plt.title(title)
-        plt.axis("off")
-        plt.show()
+        if not isinstance(image, list):
+            plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+            plt.title(title)
+            plt.axis("off")
+            plt.show()
+        elif isinstance(image, list):
+            for idx, img in enumerate(image):
+                plt.subplot(1, len(image), idx + 1)
+                plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+                plt.title(f"{title} {idx+1}")
+                plt.axis("off")
+            plt.show()
+        else:
+            print("Invalid image format for display.")
     else:
         print("No image to display.")
 
-def display_gaussian_blur(image, title="Image", kernel_size=(5, 5), sigma=1.5, display=True):
+def display_gaussian_blur(image, title="Image", kernel_size=(5, 5), sigma=0, display=True):
     blurred_image = cv2.GaussianBlur(image, kernel_size, sigma)
     if display:
         display_image(blurred_image, f"{title} - Gaussian Blur")
     return blurred_image, f"{title} - Gaussian Blur", kernel_size, sigma
+
+
+
 
 if __name__ == "__main__":
     main()

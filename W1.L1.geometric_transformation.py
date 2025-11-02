184a72973a0ceeebbe8b4edb143cbb0fcd0b37e9
Wang Lee <leew2@csp.edu>
2025-11-02 15:30:57 -0600

    Geo rotate image

--- W1.L1.geometric_transformation.py
+++ W1.L1.geometric_transformation.py
@@ -1,21 +1,31 @@
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
     images = []
-    image = load_image("shareImage/cat.jpg", title="Cat")
-    images.append(image[0])
-    display_image(image=image[0], title=image[1].__str__())
+    image = load_image("shareImage/cat.jpg")
+    images.append(image)
+    display_image(image=images[0], title="Cat")
+    rotate = rotate_image(image=images[0], angle=45)
+    images.append(rotate)
+    display_image(image=images[1], title="Rotated Image")
     pass
 
+def rotate_image(image, angle, center=None, scale=1.0):
+    (h, w) = image.shape[:2]
+    if center is None:
+        center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, scale)
+    rotated = cv2.warpAffine(image, M, (w, h))
+    return rotated
 
-
-def load_image(file_path, title="Image"):
+def load_image(file_path):
     image = cv2.imread(file_path)
     if image is None:
         print("Error loading image.")
-        return None, None
-    return image, title
+        return None
+    return image
 
 def display_image(image, title="Image"):
     if image is not None:

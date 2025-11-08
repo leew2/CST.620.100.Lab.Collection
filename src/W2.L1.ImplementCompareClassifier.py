18687fc9e2d69cb08c9adeea0c8d8d153388d462
Wang Lee <leew2@csp.edu>
2025-11-08 17:28:41 -0600

    KNN implement and metrics

--- src/W2.L1.ImplementCompareClassifier.py
+++ src/W2.L1.ImplementCompareClassifier.py
@@ -2,20 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
 
 def main():
     x, y = load_data()
     x = x.reshape(x.shape[0], -1)
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-    
 
-    pass
+    classifier = implement_classifier(x_train, y_train)
+    metrics("KNN", classifier, x_test, y_test)
+
+def metrics(name, model, x_test, y_test):
+    y_pred = model.predict(x_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f"{name} Accuracy: {accuracy:.4f}")
+
+# Implement Classifier ---------------------------------------------------------------------
+def implement_classifier(x_train, y_train):
+    classifier = KNeighborsClassifier(n_neighbors=3)
+    classifier.fit(x_train, y_train)
+    return classifier
 
 
+# End Implement Classifier -----------------------------------------------------------------
 
+# Load and view ------------------------------------------------------------------------------
 def load_data():
     digits = datasets.load_digits(return_X_y=True)
-    print_img(digits)
+    #print_img(digits)
     x, y = digits
     
     return x, y
@@ -24,7 +39,7 @@ def print_img(img):
     plt.imshow(img[0], cmap="gray")
     plt.axis("off")
     plt.show()
-
+# End Load and view --------------------------------------------------------------------------
 
 if __name__ == "__main__":
     main()

a7cb6b2f9d74b8d5cce43ba0e038c3c7ad9001c3
Wang Lee <leew2@csp.edu>
2025-11-14 17:13:23 -0600

    Alex Arch done

--- src/W3.L1.CNN_Exploration.py
+++ src/W3.L1.CNN_Exploration.py
@@ -6,12 +6,41 @@ import torchvision.transforms as transform
 import matplotlib.pyplot as plt
 
 def main():
-    loadData()
+    train, data = loadData()
 
+    alex = AlexNET()
+    print(alex)
 
 
 
     pass
+
+# CNN ---------------------------------------------------------------------------
+class AlexNET(nn.Module):
+    def __init__(self, num_class=10,):
+        super(AlexNET, self).__init__()
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1), 
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.fc_layers = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128 * 8 * 8, 512),
+            nn.ReLU(),
+            nn.Linear(512, num_class)
+        )
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = self.fc_layers(x)
+        return x
+
+
+# -------------------------------------------------------------------------------
 
 # Load Data ---------------------------------------------------------------------
 def loadData():

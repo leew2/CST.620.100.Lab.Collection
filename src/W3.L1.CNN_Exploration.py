1c18579350f2d738d348e63732c31f5cb70f9584
Wang Lee <leew2@csp.edu>
2025-11-14 17:03:51 -0600

    ignore datafolder due to datasize, Load and process CIF 10 dataset

--- src/W3.L1.CNN_Exploration.py
+++ src/W3.L1.CNN_Exploration.py
@@ -1,12 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transform
+import matplotlib.pyplot as plt
 
 def main():
-    
+    loadData()
 
 
 
 
     pass
 
+# Load Data ---------------------------------------------------------------------
+def loadData():
+    myTransform = transform.Compose([
+        transform.ToTensor(),
+        transform.Normalize((0.5), (0.5))
+    ])
+    train = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=myTransform)
+    test = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=myTransform)
+    showImg(train)
+    train = torch.utils.data.DataLoader(train, batch_size=32, shuffle=True)
+    test = torch.utils.data.DataLoader(test, batch_size=32, shuffle=False)
+    
+    return train, test
+
+def showImg(imgs):
+    plt.imshow(imgs.data[0])
+    plt.title(f"Sample Image - Class/Label: {imgs.targets[0]}")
+    plt.show()
+        
+
+
+
+# -------------------------------------------------------------------------------
 
 
 if __name__ == "__main__":
